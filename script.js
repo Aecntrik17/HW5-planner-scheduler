a798be92d1b4600c7f0aabb26ac82c133feeeb19
Aaron Sims <aaron@adsimsllc.com>
2020-06-18 17:25:19 -0400

    creating for each statement to loop through all the html searching for 'tr' elements and then grabing that data and assigning it to the specific id referenced.

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ curDateTime.append(NowMoment.format("dddd, MMMM, Do, YYYY"));
 console.log(curDateTime);
 var agendaDisplay = $("#agenda-display");
 var timeEl = $("#time");
+
+// change color of each row based on current time
+// creating a for each statement that will loop through each of the html "tr" elements
+$("tr").each(function () {
+  console.log($(this));
+  // searching for the id tag and saving that specific data to the hour variable
+  let hour = $(this).attr("id");
+
+  // comparing current time to time of slot as it runs through the loop
+  if (+hour < +moment().get("hour")) {
+    // change the color of the line, create "past class in css, present
+    $(this).addClass("past");
+    // change the color of the line, create "present class in css
+  } else if (+hour === +NowMoment.format("H")) {
+    $(this).addClass("present");
+    // change the color of the line, create "future class in css
+  } else {
+    $(this).addClass("future");
+  }
+  //   retrieving the data stored in local storage and setting it to a variable called agenda
+  var agenda = localStorage.getItem("agenda" + hour);
+
+  // taking the saved user agenda from the local storage and writing it to the page in the placeholder input
+  let agendaDisplay = $(this).children(".middle").children("textarea");
+  //   using .val so that it reads the value and not the object/element
+  agendaDisplay.val(agenda);
+});
